ffef0d8b00b33e24308758ba25c224a80ac285a5
jaeheng <jaeheng@126.com>
2020-09-23 16:13:35 +0800

    input user.email and name

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -u
 
+# input git config, user.email and user.name
+read -t 10 -p "git user.email: " email
+if [ ! $email ]
+then
+    echo 'Error: empty user.email!'
+    exit
+fi
+
+read -t 10 -p "git user.name: " name
+
+if [ ! $name ]
+then
+    echo 'Error: empty user.name!'
+    exit
+fi
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew install git
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -10,7 +26,10 @@ npm install -g nrm
 nrm use taobao
 npm install -g @vue/cli
 npm install -g gulp
-npm instlal -g http-server
+npm install -g http-server
 npm install -g typescript
+
+git config --global user.email $email
+git config --global user.name $name
 
 echo "Success, enjoy your fe life!"
