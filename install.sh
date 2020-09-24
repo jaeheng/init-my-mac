505d58c2049713525bbd5d3ae83028f889aa92e4
jaeheng <jaeheng@126.com>
2020-09-24 13:11:32 +0800

    iterm2

--- install.sh
+++ install.sh
@@ -24,11 +24,13 @@ git -C "$(brew --repo)" remote set-url origin https://mirrors.tuna.tsinghua.edu.
 
 # 以下针对 mac OS 系统上的 Homebrew
 git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+
+# 更换后测试工作是否正常
+brew update
+
 git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git
 git -C "$(brew --repo homebrew/cask-fonts)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-fonts.git
 git -C "$(brew --repo homebrew/cask-drivers)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-drivers.git
-# 更换后测试工作是否正常
-brew update
 
 # install git
 brew install git
@@ -116,7 +118,7 @@ rm -f thunder.dmg
 hdiutil eject /Volumes/迅雷/
 
 # install iTerm2
-wget -O iTerm2.zip https://iterm2.com/downloads/stable/iTerm2-3_3_12.zip
+wget -O iTerm2.zip https://raw.githubusercontent.com/jaeheng/init-my-mac/master/application/iTerm2-3_3_12.zip
 unzip iTerm2.zip
 mv iTerm.app /Applications/
 rm -f iTerm2.zip
